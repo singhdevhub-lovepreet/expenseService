9b6fafd32092f1d110e258d87765d75393b7bc3a
singhdevhub-lovepreet <lpskshorts@gmail.com>
2024-06-23 19:52:53 +0530

    Added controllers path

--- src/main/java/com/expense/service/controller/ExpenseController.java
+++ src/main/java/com/expense/service/controller/ExpenseController.java
@@ -2,19 +2,16 @@ package com.expense.service.controller;
 
 import com.expense.service.dto.ExpenseDto;
 import com.expense.service.service.ExpenseService;
-import jakarta.websocket.server.PathParam;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/expense/v1")
 public class ExpenseController
 {
 
@@ -25,8 +22,8 @@ public class ExpenseController
         this.expenseService = expenseService;
     }
 
-    @GetMapping(path = "/expense/v1/getExpense")
-    public ResponseEntity<List<ExpenseDto>> getExpense(@PathParam(value = "user_id") @NonNull String userId){
+    @GetMapping(path = "/getExpense")
+    public ResponseEntity<List<ExpenseDto>> getExpense(@RequestParam(value = "user_id") @NonNull String userId){
          try{
             List<ExpenseDto> expenseDtoList = expenseService.getExpenses(userId);
             return new ResponseEntity<>(expenseDtoList, HttpStatus.OK);
@@ -35,8 +32,8 @@ public class ExpenseController
          }
     }
 
-    @PostMapping(path="/expense/v1/addExpense")
-    public ResponseEntity<Boolean> addExpenses(@RequestHeader(value = "x-user-id") @NonNull String userId, ExpenseDto expenseDto){
+    @PostMapping(path="/addExpense")
+    public ResponseEntity<Boolean> addExpenses(@RequestHeader(value = "X-User-Id") @NonNull String userId, ExpenseDto expenseDto){
         try{
             expenseDto.setUserId(userId);
             return new ResponseEntity<>(expenseService.createExpense(expenseDto), HttpStatus.OK);
